94cdac3389b6b312ab64dc14d7946b1e0945be1b
Yaşar Emre Doğru <=>
2023-06-27 13:22:16 +0300

    store provider, bottom navigation added

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+import { Provider } from 'react-redux';
+
+import {  combineReducers, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore } from "redux";
+import waterReducer from './src/store/water-reducer'
+import ReduxThunk from 'redux-thunk';
+
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import MyTabs from './src/navigation';
+
+const rootReducer = combineReducers({
+  water: waterReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <MyTabs />
+      <StatusBar />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
